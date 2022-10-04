cf838679a449f37da4548a0e91cfa75f964174f9
Kankanala Siri <kankanalachantaasiri@gmail.com>
2022-10-04 09:57:04 +0530

    Initial commit to master

---
+++ src/main/java/com/thesiri/spring/blog/api/SpringBlogApiApplication.java
@@ -0,0 +1,13 @@
+package com.thesiri.spring.blog.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBlogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBlogApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thesiri/spring/blog/api/SpringBlogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thesiri.spring.blog.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBlogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
